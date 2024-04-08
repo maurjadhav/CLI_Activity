25135ff7ff34433dff0852103cf46b9eacb9508b
mayur jadhav <maur.r.jadhav@gmail.com>
2024-04-08 14:21:18 +0530

    added changes for reusable script

--- aws/vpc-subnet.sh
+++ aws/vpc-subnet.sh
@@ -1,42 +1,49 @@
 #!/bin/bash
 
 #Creating a vpc
-aws ec2 create-vpc --cidr-block "192.168.0.0/16" \
-    --tag-specifications "ResourceType=vpc,Tags=[{Key=Name,Value=Learning}]"
+vpc_id=$(aws ec2 create-vpc --cidr-block "192.168.0.0/16" \
+    --tag-specifications "ResourceType=vpc,Tags=[{Key=Name,Value=Learning}]" \
+    --query "Vpc.VpcId" \
+    --output text)
+echo "Vpc Id is : ${vpc_id}"
 
-#"VpcId": "vpc-04c0e96c1827467de",  vpc-01c6d889cc209bb81 
-
-aws ec2 describe-vpcs --vpc-ids --output text
 
 #Create subnet
-aws ec2 create-subnet \
-    --vpc-id "vpc-04c0e96c1827467de" \
+Web1_subnet=$(aws ec2 create-subnet \
+    --vpc-id ${vpc_id} \
     --cidr-block "192.168.0.0/24" \
     --availability-zone "ap-south-2a" \
-    --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=Web-1}]"
+    --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=Web-1}]" \
+    --query "Subnet.SubnetId" \
+    --output text)
+echo "Web1 Subnet Id is : ${Web1_subnet}"
 
- #"SubnetId": "subnet-01ba3b8cb67a5f7fc",
 
- aws ec2 create-subnet \
-    --vpc-id "vpc-04c0e96c1827467de" \
+Web2_subnet=$(aws ec2 create-subnet \
+    --vpc-id ${vpc_id} \
     --cidr-block "192.168.1.0/24" \
     --availability-zone "ap-south-2a" \
-    --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=Web-2}]"
+    --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=Web-2}]" \
+    --query "Subnet.SubnetId" \
+    --output text)
+echo "Web2 Subnet Id is : ${Web2_subnet}"
 
 
-#"SubnetId": "subnet-05ec55413ff1d3b99",
-
- aws ec2 create-subnet \
-    --vpc-id " vpc-01c6d889cc209bb81 " \
-    --cidr-block "10.0.0.0/24" \
+Data1_subnet=$(aws ec2 create-subnet \
+    --vpc-id ${vpc_id} \
+    --cidr-block "192.168.2.0/24" \
     --availability-zone "ap-south-2c" \
-    --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=Data-1}]"
+    --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=Data-1}]" \
+    --query "Subnet.SubnetId" \
+    --output text)
+echo "Web2 Subnet Id is : ${Data1_subnet}"
 
-# "SubnetId": "subnet-0aac979f07c24c5b1",
- aws ec2 create-subnet \
-    --vpc-id " vpc-01c6d889cc209bb81 " \
-    --cidr-block "10.0.1.0/24" \
+
+Data2_subnet=$(aws ec2 create-subnet \
+    --vpc-id  ${vpc_id} \
+    --cidr-block "192.168.3.0/24" \
     --availability-zone "ap-south-2a" \
-    --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=Data-2}]"
-
-#"SubnetId": "subnet-0a50a272566f8387a",
+    --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=Data-2}]" \
+    --query "Subnet.SubnetId" \
+    --output text)
+echo "Web2 Subnet Id is : ${Data2_subnet}"
